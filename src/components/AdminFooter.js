eb0b94c91712e3ddb2504d382bfeaca0c3987fe9
Sourav <souravvirtual@gmail.com>
2023-09-04 17:13:49 +0530

    Admin Hamburger menu items

---
+++ src/components/AdminFooter.js
@@ -0,0 +1,14 @@
+function AdminFooter(){
+    return(
+        <footer id="footer" className="footer">
+        <div className="copyright">
+          &copy; Copyright <strong><span>Campbooka</span></strong>. All Rights Reserved
+        </div>
+        <div className="credits">
+          Designed by <a href="#">TRD</a>
+        </div>
+      </footer>
+    )
+}
+
+export default AdminFooter;
---
+++ src/components/AdminHome.js
@@ -0,0 +1,27 @@
+function AdminHome(){
+    return(
+        <main id="main" className="main">
+
+        <div className="pagetitle">
+          <h1>Dashboard</h1>
+          <nav>
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item"><a href="index.html">Home</a></li>
+              <li className="breadcrumb-item active">Dashboard</li>
+            </ol>
+          </nav>
+        </div>
+    
+        <section className="section dashboard">
+          <div className="row">
+    
+            
+    
+          </div>
+        </section>
+    
+      </main>
+    )
+}
+
+export default AdminHome;
